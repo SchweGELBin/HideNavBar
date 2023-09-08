53e0431c2bfeed377e863d124069e108629061db
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2023-09-08 14:10:12 +0200

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -1,4 +1,4 @@
-   ##########################################################################################
+##########################################################################################
 # Custom Logic
 ##########################################################################################
 
@@ -25,138 +25,15 @@ fi
 #Find and delete conflicting overlays
 find /data/adb/modules -type d -not -path "*HideNavBar/system*" -iname "*navigationbarmodegestural*" -exec rm -rf {} \; 2>/dev/null 
 
-#Detect system language for translation 
-LANG=$(settings get system system_locales)
-LANGS=$(echo "${LANG:0:2}" )
-if [ -f "$MODPATH"/Lang/"$LANGS"/"$LANGS"1.txt ]; then
-    :
-else
-    LANGS=en
-fi
-
-LNG="$MODPATH"/Lang/"$LANGS"/"$LANGS"
-
-#Standard volume selector stuff but with translations
-#Fullscreen or immersive selection
-cat "$LNG"1.txt
-if $VKSEL; then
-     BH=0.0
-     FBH=0
-     FFH=0
-     FH=0.0
-     SS=true
-     FIM=false
-     VAR3=a
-     VAR4=a
-else
-     FH=48.0
-     FBH=0
-     FFH=9500
-     BH=0.0
-     FFS=false
-     FIM=true
-     SS=true
-fi 
-
-#Hide pill
-if [ "$FH" = 48.0 ] ; then
-     cat "$LNG"2.txt
-     if $VKSEL; then
-     VAR3=HP
-     HD=true
-     else
-     VAR3=a
-     fi 
-fi
-
-if [ "$API" -le 30 ] ; then
- if [ "$FH" = 48.0 ] ; then
-  cat "$LNG"8.txt
-  if $VKSEL; then
-  VAR4=HKB
-  else
-  VAR4=a
-  fi
- fi
-else
- :
-fi
-
-#Small keyboard bar
-if [ "$FH" = 48.0 ] ; then
-     cat "$LNG"3.txt
-     if $VKSEL; then
-     FH=16.0
-     FFH=4000
-     else
-     :
-     fi 
-fi
-
-#Gesture sensitivity 
-if [ "$SS" = true ] ; then
-     cat "$LNG"4.txt
-     if $VKSEL; then
-     GS=18.0
-     FGS=4000
-     else
-     GS=32.0
-     FGS=9000
-     fi
-fi
-
-if [ "$SS" = true ] ; then
-     cat "$LNG"9.txt
-     if $VKSEL; then
-      if [ "$FFH" = 0 ] ; then
-      FBH=300
-      FFH=300
-      else
-      FBH=300
-      fi
-     else
-      :
-     fi
-fi
-
-#Disable back gesture on Q
-if [ "$API" -eq 29 ] && [ "$FH" = 0.0 ] ; then
-     cat "$LNG"5.txt
-     if $VKSEL; then
-     cp -rf "$MODPATH"/Mods/DBGQ/* "$MODPATH"
-     else
-     :
-     fi
-fi
-
-#Disable back gesture on R+
-#Reenable back gesture if no is selected 
-if [ "$API" -ge 30 ] ; then
-     cat "$LNG"5.txt
-     if $VKSEL; then
-     DBG=true
-     else
-     DBG=false
-     settings delete secure back_gesture_inset_scale_left &>/dev/null
-     settings delete secure back_gesture_inset_scale_right &>/dev/null
-     fi
-fi     
-
-#Left or both sides 
-if [ "$DBG" = true ] ; then
-     cat "$LNG"6.txt
-     if $VKSEL; then
-     settings put secure back_gesture_inset_scale_left -1 &>/dev/null
-     else
-     settings put secure back_gesture_inset_scale_left -1 &>/dev/null
-     settings put secure back_gesture_inset_scale_right -1 &>/dev/null
-     fi
-fi
-
-#Back gesture warning 
-if [ "$DBG" = true ] ; then
-    cat "$LNG"7.txt
-fi    
+#Fullscreen values
+BH=0.0
+FBH=0
+FFH=0
+FH=0.0
+SS=true
+FIM=false
+VAR3=a
+VAR4=a 
 
 #Write to overlay resources
 RES="$MODPATH"/Mods/Qtmp/res/values/dimens.xml
