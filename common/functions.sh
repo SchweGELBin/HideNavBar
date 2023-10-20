b13612d93236b71f9f2c1f663db5bdb3c23e847f
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2023-10-20 23:19:39 +0200

    Update functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -146,11 +146,8 @@ mount_mirrors() {
   done
 }
 
-# Credits
-ui_print "**************************************"
-ui_print "*   MMT Extended by Zackptg5 @ XDA   *"
-ui_print "**************************************"
-ui_print " "
+# Credits - MMT Extended by Zackptg5 @ XDA
+
 
 # Check for min/max api version
 [ -z $MINAPI ] || { [ $API -lt $MINAPI ] && abort "! Your system API of $API is less than the minimum api of $MINAPI! Aborting!"; }
